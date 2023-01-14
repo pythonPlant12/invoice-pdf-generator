3a925c016a1483c8cc6f75b7a2f9e2aa2202b7c8
Nikita Lutsai <105123083+nikitalutsai@users.noreply.github.com>
2023-01-14 19:25:22 +0100

    Add a table

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*")
 # Iterate over all the objects in directory
 for file in filepaths:
-    # Create a variable of a read_excel function, you should define a sheet_name and file
+    # Create a variable of a read dataframe of an Excel file, you should define
+    # a sheet_name and file
     df = pd.read_excel(file, sheet_name="Sheet 1")
     # Create a pdf instance with FPDF class
     pdf = FPDF("P", unit="mm", format="A4")
@@ -24,6 +25,32 @@ for file in filepaths:
     # Create a pdf file in PDFs directory using filename and f-string
     pdf.set_font(family="Times", size=16, style="B")
     # Create a cell for a new input
-    pdf.cell(w=50, h=10, txt=f"Date: {date}")
+    pdf.cell(w=50, h=10, txt=f"Date: {date}", ln=1)
+
+    # Add a header
+    columns = df.columns
+    # Use list comprehension to rid out of underscore and capitalize
+    columns = [item.replace("_", " ").capitalize() for item in columns]
+    pdf.set_font(family="Times", style="B", size=8)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    # Add rows
+    # Iterate over rows in df file
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=8)
+        pdf.set_text_color(80, 80, 80)
+    # We create cells, but we should convert txt=in string as row["product_id"] is int and
+    # we'll get this error AttributeError: 'int' object has no attribute 'replace'
+    # We add border here with border=1, and ln=1 to break the line
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
     # Create a pdf file in PDFs directory using filename and f-string
     pdf.output(f"PDFs/{filename}.pdf")
